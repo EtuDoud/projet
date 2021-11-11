451512087424bb400dbdb032fe468798b33af81a
EtuDoud <bouraimawadoud@gmail.com>
2021-11-11 19:39:50 +0100

    Mon premier Projet React

---
+++ src/Todo/TodoList.js
@@ -0,0 +1,98 @@
+// import React from 'react'
+
+// const TodoList = () => {
+    
+
+//     return (
+//         <div>
+//             <h1>Todo List</h1>
+//             <form>
+//                 <input type="text" placeholder="Renseignez un item" />
+//                 <button>Ajouter</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default TodoList
+
+
+import React, { Component } from 'react'
+
+ class TodoList extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            userInput : '',
+            items : []
+        }
+    }
+
+    onChange(event){
+        this.setState ( {
+            userInput : event.target.value
+        },);
+    }
+
+    addTodo(event){
+        event.preventDefault()
+        this.setState (
+            {   
+                userInput : '',
+                items : [...this.state.items, this.state.userInput]
+            }
+        )
+    }
+
+    deleteTodo(event){
+        event.preventDefault()
+        const array = this.state.items
+        const index = array.indexOf(event.target.value)
+        array.splice(index,1)
+
+        this.setState (
+            {
+                items : array
+            }
+        )
+    }
+
+    renderTodos(){
+        return this.state.items.map( (item) => {
+            return(
+                <div className="list-group-item"> 
+                    {item} | <button onClick={this.deleteTodo.bind(this)} >X</button>
+                </div>
+            )
+        } )
+    }
+
+     render(){
+         return(
+             <div>
+                 <h1 align="center">Ma Todo List</h1>
+                 <form className="form-row align-items-center">
+                    <input 
+                    value={ this.state.userInput } 
+                    type="text" 
+                    placeholder="Renseignez un item" 
+                    onChange={ this.onChange.bind(this) }
+                    className="form-control mb-2"
+                    />
+                    <button 
+                    onClick = {this.addTodo.bind(this)} 
+                    className = "btn btn-primary"
+                    >
+                        Ajouter
+                    </button>
+                 </form>
+                 <div className="list-group">
+                     { this.renderTodos() }
+                 </div>
+             </div>
+         );
+     }
+ }
+
+ export default TodoList;
